4e598c97896b033fcb2bc00e79ca51cd94c578af
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-09-16 10:42:53 +0900

    until index and create for log in user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,39 @@
 class ArticlesController < ApplicationController
-  def show
-    @str = "クロちゃんかわいい"
+  before_action :move_to_index, except: :index
+
+  def index
+    @articles = Article.all.order("id DESC").page(params[:page]).per(4)
   end
+  
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    #
+  end
+
+  def edit
+    #@article = Article.findその記事のid
+  end
+
+  def update
+    #
+  end
+
+  def destroy
+    @current_user_id = current_user.id
+    #Article.delete(v)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:name, :title, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
